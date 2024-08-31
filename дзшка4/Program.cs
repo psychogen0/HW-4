b3dbbc379ed35cbf40dcb6ad01c618b88fc82aca
Natalia <natabilcuk@gmail.com>
2024-08-31 14:42:43 +0300

    Add project files.

---
+++ дзшка4/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Enter first number");
+int firstNum = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Enter second number");
+int secondNum = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Enter action you want to perform");
+char action = char.Parse(Console.ReadLine());
+
+int result = 0;
+
+switch (action)
+{
+    case '+':
+        result = firstNum + secondNum;
+        break;
+    case '-':
+        result = firstNum - secondNum;
+        break;
+    case '*':
+        result = firstNum * secondNum;
+        break;
+    case '/':
+        result = firstNum / secondNum;
+        break;
+}
+Console.WriteLine();
+Console.WriteLine($"{firstNum} {action} {secondNum} = {result}");
